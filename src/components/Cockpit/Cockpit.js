fb1ffc7ed7adebce1c96156607a6102b24437425
VTI\duc.nguyenviet <nguyenvietduc030496@gmail.com>
2021-04-09 13:53:51 +0700

    88

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -5,10 +5,10 @@ const cockpit = (props) => {
     const assignedClasses = [];
     let btnClass = '';
     if (props.showPersons) {
-        btnClass = classes.red; 
+        btnClass = classes.Red; 
     }
     if (props.persons.length <= 2) {
-        assignedClasses.push(classes.Red);
+        assignedClasses.push(classes.red);
     }
     if (props.persons.length <= 1) {
         assignedClasses.push(classes.bold);
@@ -16,7 +16,7 @@ const cockpit = (props) => {
 
     return (
         <div className={classes.cockpit}>
-            <h1>Hi, I'm a React App</h1>
+            <h1>{props.title}</h1>
             <p className={assignedClasses.join(' ')}>This is really working!</p>
             <button className={btnClass}
                 onClick={props.clicked}>
--- src/containers/App.js
+++ src/containers/App.js
@@ -69,6 +69,7 @@ class App extends Component {
     return (
       <div className='App'>
         <Cockpit
+          title={this.props.appTitle}
           showPersons={this.state.showPersons}
           persons={this.state.persons}
           clicked={this.togglePersonHandler}>
