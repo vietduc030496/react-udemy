75653a1789d11008e0d60d1ae99c33375ec099fc
vietduc030496 <nguyenvietduc030496@gmail.com>
2021-04-11 22:27:43 +0700

    102

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -4,13 +4,12 @@ import './Person.css'
 class Person extends Component {
     render() {
         console.log('[Person.js] rendering....');
-        return (
-            <div className='Person'>
-                <p onClick={this.props.click}>I'm a {this.props.name}! I'm {this.props.age} year old</p>
-                <p>{this.props.children}</p>
-                <input type='text' onChange={this.props.changed}
-                    value={this.props.name}></input>
-            </div>
+        return ([
+            <p key='i1' onClick={this.props.click}>I'm a {this.props.name}! I'm {this.props.age} year old</p>,
+            <p key='i2'>{this.props.children}</p>,
+            <input key='i3' type='text' onChange={this.props.changed}
+                value={this.props.name}></input>
+        ]
         )
     };
 }
